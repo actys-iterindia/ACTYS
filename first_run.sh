95025c29e2e7a0961ec7be151a8f9996a3643a78
actys <actys@iterindia.in>
2026-01-16 16:01:55 +0530

    updated first_run.sh to set license file path

--- first_run.sh
+++ first_run.sh
@@ -1,18 +1,26 @@
-echo "--------------> Define ACTYS_HOME variable where Data folder is located e.g."
 echo ""
-
-echo "             export ACTYS_HOME=/home/${USER}/ACTYS"
+echo "Provide name and full path of actys license file. e.g. $HOME/actys_licesne_xx"
+read actys_license
 echo "" 
+echo ""
+echo "--------------> Define ACTYS_HOME variable where Data folder is located e.g."
+echo""echo "" 
+echo "             export ACTYS_HOME=/home/${USER}/ACTYS"
 echo ""
 echo "=============> Enter the path for ACTYS_HOME variable"
 read act_home
 echo "export ACTYS_HOME=$act_home" >> ~/.bashrc	
+echo "Check in the .bashrc file if ACTYS_HOME is defined"
 tail ~/.bashrc
 echo ""
 echo "ACTYS_HOME is defined as $act_home" 
+echo ""
 cd ${act_home}/Data
+echo""
+echo "Copying license file to $act_home/Data"
+cp $actys_license ${act_home}/Data/.
 echo "Downloading EAF2010 libraries"
-#wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/EAF2010data.tar.bz2
+wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/EAF2010data.tar.bz2
 echo "Doing untar of libraries"
 tar --extract --file=EAF2010data.tar.bz2 $(tar -tf EAF2010data.tar.bz2 | grep fus)
 mv EAF2010data EAF2010
@@ -21,8 +29,8 @@ find EAF2010 -type f -name '*fus*20100*' | while read f; do
     mv "$f" "$newname"
 done
 echo "Downloading bin boundaries"
-#wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/ebins.tar.bz2
-#tar -xvjf ebins.tar.bz2
+wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/ebins.tar.bz2
+tar -xvjf ebins.tar.bz2
 echo "Doing example run to test"
 echo "Creating run directory if does not exits"
 mkdir -p ${act_home}/run
