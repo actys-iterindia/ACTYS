0edb3370b050f38a80172686d664ce29ec4ad378
actys <actys@iterindia.in>
2026-01-16 09:50:41 +0530

    updated cross-section library path to 2010

--- first_run.sh
+++ first_run.sh
@@ -12,15 +12,23 @@ echo ""
 echo "ACTYS_HOME is defined as $act_home" 
 cd ${act_home}/Data
 echo "Downloading EAF2010 libraries"
-wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/EAF2010data.tar.bz2
+#wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/EAF2010data.tar.bz2
 echo "Doing untar of libraries"
-tar -xvjf EAF2010data.tar.bz2
+tar --extract --file=EAF2010data.tar.bz2 $(tar -tf EAF2010data.tar.bz2 | grep fus)
+mv EAF2010data EAF2010
+find EAF2010 -type f -name '*fus*20100*' | while read f; do
+    newname=$(echo "$f" | sed 's/20100/2010/g')
+    mv "$f" "$newname"
+done
 echo "Downloading bin boundaries"
-wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/ebins.tar.bz2
-tar -xvjf ebins.tar.bz2
+#wget https://git.oecd-nea.org/fispact/nuclear_data/-/blob/main/ebins.tar.bz2
+#tar -xvjf ebins.tar.bz2
 echo "Doing example run to test"
+echo "Creating run directory if does not exits"
+mkdir -p ${act_home}/run
 cd ${act_home}/run
 cp ${act_home}/examples_linux/CrW ${act_home}/run/.
-cp ${act_home}/examples_linux/EU-FW ${act_home}/run/.
+cp ${act_home}/examples_linux/tripoli_flux ${act_home}/run/.
+echo './actyslinux CrW'
 
 
